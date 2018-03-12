9bd7dc10c3f3813bad678c9d2440cf755ba7e762
larry stent <lawrencestent@gmail.com>
2018-03-12 23:06:38 +0200

    add response object and tv show models

--- app/src/main/java/com/hiero/lawrencestent/tvlister/model/ShowModel.kt
+++ app/src/main/java/com/hiero/lawrencestent/tvlister/model/ShowModel.kt
@@ -3,5 +3,8 @@ package com.hiero.lawrencestent.tvlister.model
 /**
  * Created by lawrencestent on 2018/03/12.
  */
-class ShowModel {
+class ShowModel(private val original_name: String? = "",
+                private val vote_average: Double = 0.0,
+                private val poster_path : String? = "",
+                private val overview: String? = "") {
 }
---
+++ app/src/main/java/com/hiero/lawrencestent/tvlister/model/TvShowResponse.kt
@@ -0,0 +1,7 @@
+package com.hiero.lawrencestent.tvlister.model
+
+/**
+ * Created by lawrencestent on 2018/03/12.
+ */
+data class TvShowResponse(private val results : List<ShowModel> ) {
+}
