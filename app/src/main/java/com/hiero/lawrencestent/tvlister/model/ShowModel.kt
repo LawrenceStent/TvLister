8cb9e3ad8de847b9e1dbccf9e0c33cdcc5d03903
larry stent <lawrencestent@gmail.com>
2018-03-14 23:43:20 +0200

    add id field to model

--- app/src/main/java/com/hiero/lawrencestent/tvlister/model/ShowModel.kt
+++ app/src/main/java/com/hiero/lawrencestent/tvlister/model/ShowModel.kt
@@ -5,7 +5,8 @@ import java.io.Serializable
 /**
  * Created by lawrencestent on 2018/03/12.
  */
-class ShowModel(val original_name: String? = "",
+class ShowModel(val id : Int? = 0,
+                val original_name: String? = "",
                 val vote_average: Double = 0.0,
                 val poster_path : String? = "",
                 val overview: String? = "") : Serializable{
