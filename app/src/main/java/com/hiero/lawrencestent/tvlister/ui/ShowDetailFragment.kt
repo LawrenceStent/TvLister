91150b0e249e9264063c56516c454ea70ac88822
larry stent <lawrencestent@gmail.com>
2018-03-18 21:17:51 +0200

    add big hero image, and refactor code

--- app/src/main/java/com/hiero/lawrencestent/tvlister/ui/ShowDetailFragment.kt
+++ app/src/main/java/com/hiero/lawrencestent/tvlister/ui/ShowDetailFragment.kt
@@ -1,16 +1,19 @@
 package com.hiero.lawrencestent.tvlister.ui
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.hiero.lawrencestent.tvlister.R
 import com.hiero.lawrencestent.tvlister.model.ShowModel
 import com.hiero.lawrencestent.tvlister.service.TvShowService
@@ -19,6 +22,13 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_show_detail.*
+import android.support.v4.view.ViewCompat.setAlpha
+import android.widget.*
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import kotlinx.android.synthetic.main.viewholder_similar_show.view.*
+
 
 /**
  * Created by lawrencestent on 2018/03/12.
@@ -45,40 +55,89 @@ class ShowDetailFragment : Fragment() {
 
     var showDetailModel: ShowModel? = null
     var tvShowService: TvShowService? = null
+
     var similarShowAdapter : SimilarShowAdapter? = null
     var getShowProgress: ProgressBar? = null
     var similarShowList: RecyclerView? = null
 
+    var title : TextView? = null
+    var overview: TextView? = null
+    var rating: TextView? = null
+    var heroRootLayout: FrameLayout? = null
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = inflater?.inflate(R.layout.fragment_show_detail, container, false)
 
-        Log.d(TAG, "Opens fragment")
         var args = arguments
         showDetailModel = args.getSerializable(SHOW_DETAIL) as ShowModel
-        
-
         tvShowService = TvShowService(resources)
 
-        val title = view?.findViewById<TextView>(R.id.show_detail_title)
-        val overview = view?.findViewById<TextView>(R.id.show_detail_overview)
-        val rating = view?.findViewById<TextView>(R.id.show_detail_rating)
-        similarShowList  = view?.findViewById<RecyclerView>(R.id.list_similar_shows)
-        similarShowList?.visibility = View.GONE
 
-        getShowProgress = view?.findViewById<ProgressBar>(R.id.similar_progress)
-        getShowProgress?.visibility = View.VISIBLE
+        initToolbar(view!!, showDetailModel!!)
 
-        title?.text = showDetailModel?.original_name
-        overview?.text = showDetailModel?.overview
-        rating?.text = showDetailModel?.vote_average.toString()
+        initViews(view, showDetailModel!!)
 
+        initSimilarAdapter(view.context, similarShowList!!)
+
+        initHeroImage(view, showDetailModel!!)
 
         getSimilarTvShows(tvShowService!!, showDetailModel?.id!!)
 
-        initSimilarAdapter(view?.context, similarShowList!!)
-
         return view
 
+    }
+
+    fun initToolbar(view: View, showModel: ShowModel){
+        (view.context as AppCompatActivity).setSupportActionBar(view.findViewById<Toolbar>(R.id.toolbar_detail))
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_detail)
+        toolbar.setTitle(showModel.original_name)
+        toolbar.setNavigationIcon(view.context.resources.getDrawable(R.drawable.ic_arrow_back_black_24dp))
+
+        toolbar.setNavigationOnClickListener {
+            fragmentManager.popBackStack()
+        }
+    }
+
+    fun initViews(view: View, showDetailModel: ShowModel) {
+//        title = view.findViewById<TextView>(R.id.show_detail_title)
+        overview = view.findViewById<TextView>(R.id.show_detail_overview)
+        rating = view.findViewById<TextView>(R.id.show_detail_rating)
+        similarShowList = view.findViewById<RecyclerView>(R.id.list_similar_shows)
+
+
+        similarShowList?.visibility = View.GONE
+
+        getShowProgress = view.findViewById<ProgressBar>(R.id.similar_progress)
+        getShowProgress?.visibility = View.VISIBLE
+
+//        title?.text = showDetailModel.original_name
+        overview?.text = showDetailModel.overview
+        rating?.text = showDetailModel.vote_average.toString()
+//
+        overview?.background = view.context.getDrawable(R.color.white)
+        overview?.alpha = 0.8f
+    }
+
+    fun initHeroImage(view: View, show: ShowModel){
+        heroRootLayout = view.findViewById(R.id.big_hero_root) as FrameLayout
+        heroRootLayout?.alpha = 0.95f
+
+        Picasso.with(view.context)
+                .load(view.resources?.getString(R.string.image_base_url) + show.poster_path)
+                .into(object : Target {
+
+                            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+                            }
+
+                            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                                heroRootLayout!!.background = BitmapDrawable(bitmap)
+                            }
+
+                            override fun onBitmapFailed(errorDrawable: Drawable?) {
+                                Log.e(TAG, "Hero Background Failed")
+                            }
+                })
     }
 
     fun getSimilarTvShows(tvShowService: TvShowService, id: Int){
