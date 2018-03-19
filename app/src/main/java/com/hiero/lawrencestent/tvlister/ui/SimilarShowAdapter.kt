15b99605d3bcad8609021f35245df6d583be81c9
larry stent <lawrencestent@gmail.com>
2018-03-19 23:12:57 +0200

    open show fragment on similar list click

--- app/src/main/java/com/hiero/lawrencestent/tvlister/ui/SimilarShowAdapter.kt
+++ app/src/main/java/com/hiero/lawrencestent/tvlister/ui/SimilarShowAdapter.kt
@@ -1,8 +1,12 @@
 package com.hiero.lawrencestent.tvlister.ui
 
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
+import com.hiero.lawrencestent.tvlister.R
 import com.hiero.lawrencestent.tvlister.model.ShowModel
+import kotlinx.android.synthetic.main.viewholder_similar_show.view.*
 
 /**
  * Created by lawrencestent on 2018/03/14.
@@ -23,10 +27,23 @@ class SimilarShowAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var newHolder = holder as SimilarShowViewHolder
 
         newHolder.updateShowDetail(similarShowList[position])
+
+        newHolder.itemView.similar_show_card.setOnClickListener {view ->
+            openShowDetailFragment(view, similarShowList[position])
+        }
     }
 
     fun updateSimilarShowList(similarShows: List<ShowModel>){
         this.similarShowList = similarShows.toMutableList()
         notifyDataSetChanged()
+    }
+
+    fun openShowDetailFragment(view: View, showDetail: ShowModel){
+        val fragment = ShowDetailFragment.newInstance(showDetail)
+
+        (view.context as AppCompatActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_root, fragment, ShowDetailFragment.TAG)
+                .addToBackStack(null)
+                .commit()
     }
 }
